8a62fb51d164575ff9bd33d6f86d159489f753eb
islam <islam.ali137@gmail.com>
2023-08-13 17:20:37 -0700

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar ,2015-10-19"
+ *                followed by a new line , to standard error.
+ *
+ * Return: Always 1 (Success)
+ */
+
+
+int main(void)
+{
+	printf("and that piece of art is useful - Dora Korpar, 2015- 10 - 19\n");
+	return (1);
+}
