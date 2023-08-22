5f8b25423faf3588195e96a6331feeed07b70f14
islam <islam.ali137@gmail.com>
2023-08-22 12:54:06 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all the number of base 16 in lowercase
+ *
+ * Return: (0) Always
+ */
+int main(void)
+{
+	int = alphabet;
+	for (alphabet = 0; alphabet <= 10; alphabet++)
+		putchar(alphabet);
+	for (alphabet = 'a'; alphabet < 'f'; alphabet++)
+		putchar(alphabet);
+	putchar("\n");
+	return (0);
+}
