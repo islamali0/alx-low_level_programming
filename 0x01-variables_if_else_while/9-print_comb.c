ce45f7d79c645ebfa809e61f0ef352ce7b8885b1
islam <islam.ali137@gmail.com>
2023-08-22 13:31:16 -0700

    digit combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,12 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x < 10; x++)
+	for (x = 0; x <= 9; x++)
+	{
 		putchar(x);
-	putchar(',');
-	putchar(' ');
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
