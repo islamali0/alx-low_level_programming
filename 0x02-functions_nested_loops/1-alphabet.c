fb50aea87dfd627c1dcabc05b6576ce6ba5106c7
islam <islam.ali137@gmail.com>
2023-08-25 15:38:29 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * main - print alphabet letters
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar("alphabet\n");
+
+	return (0);
+}
