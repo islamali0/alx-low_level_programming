75f43677a38d80a82db2cb348188962b7ec781b0
islam <islam.ali137@gmail.com>
2023-09-03 15:55:08 -0700

    reversed char

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,6 +9,6 @@ void _puts_recursion(char *s)
         if (*s)
         {
                 _putchar(*s);
-                _puts_rev_recursion(s + 1);
+                _print_rev_recursion(s + 1);
 	}
 }
