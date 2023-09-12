19ce739d23dae5645b87fccdfa2a175eed4dfc4c
islam <islam.ali137@gmail.com>
2023-09-12 11:29:45 -0700

    dog typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+ * File: dog.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a new type struct dog.
+ */
+
 /**
- * struct dog - a dog struct
- * name dog name
- * age : dog age
- * owner : dog owner
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 struct dog
 {
