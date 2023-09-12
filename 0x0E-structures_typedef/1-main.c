2ebbafe1bfab0bc622fdd10052c33aba5e10a693
islam <islam.ali137@gmail.com>
2023-09-12 11:53:25 -0700

    init dog

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include "dog.h"
+#include "../dog.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
@@ -10,8 +10,7 @@ int main(void)
 {
 	struct dog my_dog;
 
-	init_dog(&my_dog, "Poppy", 3.5, "Bob");
+	init_dog(&my_dog, "Django", 3.5, "Jay");
 	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-
 	return (0);
 }
